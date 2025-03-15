46b33dc09df9fb829c1c6ea09bb7b16d518501c8
Nour Saif Yassine <nour.saif.yassine@gmail.com>
2025-03-15 10:52:07 +0100

    change: popup

--- TimeTracker.ps1
+++ TimeTracker.ps1
@@ -2,7 +2,6 @@ function Get-Tracker {
     $inputWorkTime = [int](Read-Host "How long do you want to work?: ")
     $endTime = (Get-Date).AddMinutes($inputWorkTime)
     $endTimeString = $endTime.ToString("HH:mm:ss")
-    $wshell = New-Object -ComObject Wscript.Shell
     Write-Host "End time: $endTimeString"
     while($true) {
         $currentTime = Get-Date
@@ -10,7 +9,7 @@ function Get-Tracker {
         Write-Host "Current time: $currentTimeString"
         if($currentTime -ge $endTime) {
             Write-Host "Time's up!"
-            $wshell.Popup("This is a popup message!", 0, "Popup Time Tracker", 0x0)
+            Add-Type -AssemblyName PresentationFramework[System.Windows.MessageBox]::Show("This is a popup message for you to stop working!")
             break
         }
         Start-Sleep -Seconds 1
