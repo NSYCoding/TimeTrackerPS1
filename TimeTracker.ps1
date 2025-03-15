c8d2ef5682e993c8ea0f305898aa8f202a6b4296
Nour Saif Yassine <nour.saif.yassine@gmail.com>
2025-03-15 12:47:35 +0100

    add: Forced Task

--- TimeTracker.ps1
+++ TimeTracker.ps1
@@ -1,22 +1,44 @@
 function Get-Tracker {
-    $inputWorkTime = [int](Read-Host "How long do you want to work?: ")
+    param (
+        [string]$ProcessName
+    )
+    $inputWorkTime = [int](Read-Host "How many minutes do you want to work?: ")
     $endTime = (Get-Date).AddMinutes($inputWorkTime)
     $endTimeString = $endTime.ToString("HH:mm:ss")
     Write-Host "End time: $endTimeString"
-    while($true) {
+    $currentTime = Get-Date
+    $currentTimeString = $currentTime.ToString("HH:mm:ss")
+    while ($true) {
         $currentTime = Get-Date
-        $currentTimeString = $currentTime.ToString("HH:mm:ss")
-        Write-Host "Current time: $currentTimeString"
-        Write-Host "Time left: $(($endTime - $currentTime).ToString("mm\:ss"))"
-        if($currentTime -ge $endTime) {
+        if ($currentTime -ge $endTime) {
             Write-Host "Time's up!"
             Add-Type -AssemblyName PresentationFramework
             [System.Windows.MessageBox]::Show("This is a popup message for you to stop working!")
+            Clear-Host
             break
+        } else {
+            if (-not (Get-Process -Name $ProcessName -ErrorAction SilentlyContinue)) {
+                Write-Host "Process $ProcessName has stopped. Restarting..."
+                Start-Process -FilePath $ProcessName -ErrorAction Stop
+            }
+            $currentTimeString = $currentTime.ToString("HH:mm:ss")
+            Write-Host "Current time: $currentTimeString"
+            Write-Host "Time left: $(($endTime - $currentTime).ToString("mm\:ss"))"
+            Start-Sleep -Seconds 1
+            Clear-Host
         }
-        Start-Sleep -Seconds 1
-        Clear-Host
     }
+    return $currentTimeString
 }
 
-Get-Tracker
+function Set-TimeTrackerForWork {
+    $processName = Read-Host "Enter the process name you want to track: "
+    Write-Host "Starting time tracker for $processName"
+    if (-not (Get-Process -Name $processName -ErrorAction SilentlyContinue)) {
+        Write-Host "Process $processName is not running. Starting it now..."
+        Start-Process -FilePath $processName -ErrorAction Stop
+    }
+    Get-Tracker -ProcessName $processName
+}
+
+Set-TimeTrackerForWork
